4b01f5fb6aaa4efeada84f64e899cfebd0205243
csvenke <csvenke@users.noreply.github.com>
2025-01-02 18:13:04 +0100

    feat(templates): update default flake template

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -1,22 +1,19 @@
 {
-  description = "My flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = { pkgs, ... }: {
-        packages = {
-          default = pkgs.hello;
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells = {
+            default = pkgs.mkShell { };
+          };
         };
-
-        devShells = {
-          default = pkgs.mkShell { };
-        };
-      };
     };
 }
