fa454a842de0f099663173702cf1f608eef3e3a0
csvenke <csvenke@users.noreply.github.com>
2025-04-11 18:57:26 +0200

    feat(template): update flake template for easier access to devshells

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -2,17 +2,41 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    devkit = {
+      url = "github:csvenke/devkit";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ flake-parts, nixpkgs, ... }:
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      devkit,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem =
-        { pkgs, ... }:
+        { pkgs, system, ... }:
+        let
+          devShell = name: devkit.devShells.${system}.${name};
+        in
         {
           devShells = {
-            default = pkgs.mkShell { };
+            default = pkgs.mkShell {
+              name = "devkit";
+              inputsFrom = [
+                (devShell "node")
+              ];
+              packages = with pkgs; [
+                lolcat
+                figlet
+              ];
+              shellHook = ''
+                figlet "Devkit" | lolcat
+              '';
+            };
           };
         };
     };
