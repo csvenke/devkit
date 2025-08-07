d59794cc7968437ad6da3384fd4f09c7254d002c
csvenke <csvenke@users.noreply.github.com>
2025-08-07 09:44:26 +0200

    feat(python): enhance python dev shell with library path support
    * add stdenv.cc.cc to library path for native dependencies
    * set LD_LIBRARY_PATH in shell hook for runtime linking

--- devShells/python/package.nix
+++ devShells/python/package.nix
@@ -1,10 +1,27 @@
-{ pkgs }:
+{
+  pkgs,
+  lib,
+  stdenv,
+  python3,
+}:
+
+let
+  libraryPath = lib.makeLibraryPath [
+    stdenv.cc.cc
+  ];
+  python = python3.withPackages (ps: [
+    ps.pip
+    ps.pipx
+  ]);
+in
 
 pkgs.mkShell {
-  packages = with pkgs; [
-    (python3.withPackages (ps: [ ps.pip ps.pipx ]))
-    pyright
-    ruff
+  packages = [
+    python
+    pkgs.pyright
+    pkgs.ruff
   ];
+  shellHook = ''
+    export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${libraryPath}"
+  '';
 }
-
