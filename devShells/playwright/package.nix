b3bbce4f0a1441be376710a419cfac561979db87
csvenke <csvenke@users.noreply.github.com>
2026-03-03 15:59:13 +0100

    feat(devShells): add Playwright development shell configuration

---
+++ devShells/playwright/package.nix
@@ -0,0 +1,13 @@
+{ pkgs }:
+
+pkgs.mkShell {
+  packages = [
+    pkgs.playwright-driver.browsers
+  ];
+
+  shellHook = /* bash */ ''
+    export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
+    export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
+    export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=1
+  '';
+}
